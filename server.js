80a5e8b5d342ad42a4b23d37ab59436c298ad729
HirokiMiyaoka <hiroki.miyaoka@gmail.com>
2016-11-04 16:33:22 +0900

    説明の追加

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+// Expressのモジュール読み込み
 var express = require('express');
+
 var app     = express();
 
-// 3080番を指定
+// 環境変数PORTが設定されていなければ、3080番を指定
 var port = process.env.PORT || 3080;
 
 //========================================
@@ -12,16 +14,20 @@ var port = process.env.PORT || 3080;
 var router = express.Router();
 
 router.use( function( req, res, next ) {
+	// ざっくりと説明。
+	// reqの中には、リクエスト周りのデータが入っている。
+	// resはレスポンスを返すための命令が入っている。
+	// nextは次に処理を回す。
 	next();
 });
 
 // http://localhost:3000/api/ へのアクセス。
-router.get( '/', function(req, res) {
+router.get( '/', function( req, res, next ) {
 	res.json( { message: 'Successfully Posted a test message.' } );
 });
 
 // http://localhost:3000/api/value/任意の値 へのアクセス。
-router.get( '/value/:val', function(req, res) {
+router.get( '/value/:val', function( req, res, next ) {
 	// :VAL で指定した値は req.params.VAL で取得できる。
 	res.json( { message: 'Get value: ' + req.params.val, value: req.params.val } );
 });
@@ -35,10 +41,11 @@ router.get( '/value/:val', function(req, res) {
 // /で終わる場合、index.htmlを読み込むのがWebサーバーのデフォルト挙動として多いので、それにも対応している。
 // 具体的には http://localhost:3080/ にアクセスすると public/index.html にアクセスする。
 app.use( express.static( './public/' ) );
+
 // /api以下のルートを登録。
 // http://localhost:3080/api/... のアクセスはこちらになる。
 app.use('/api', router);
 
 //サーバ起動
 app.listen(port);
-console.log('listen on port ' + port);
+console.log(　'listen on port ' + port　);
